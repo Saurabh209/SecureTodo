16f69f3b34be595de43cd2c932df1fdbe4d13d96
Saurabh209 <saurabhhh2613@gmail.com>
2025-10-25 09:48:36 +0530

    started working on frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,19 @@
 
-
-
 import React from 'react'
+import Login from '../src/Login/Login'
+import { Routes, Route } from 'react-router-dom'
+import Registration from './Registration/Registration'
+import Home from './Home/Home'
 
 function App() {
   return (
-    <h1>I am AM</h1>
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/login" element={<Login />} />
+      <Route path='/registration' element={<Registration />} />
+      <Route path = '/home' element={<Home/>}/>
+      {/* <Route path="/about" element={<About />} /> */}
+    </Routes>
   )
 }
 export default App
---
+++ Frontend/src/Registration/Registration.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import './Registration.scss'
+
+export default function Registration() {
+    return (
+        <div className="login-page-container" style={{
+            backgroundImage: "url('/img/Login_Background.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+        }}>
+            <div className="content">
+                <div className="text">register</div>
+
+                <form className="registration-form">
+                    {/* Email Field */}
+                    <div className="field">
+                        <span className="span">
+                            <svg
+                                xmlSpace="preserve"
+                                viewBox="0 0 512 512"
+                                height="20"
+                                width="50"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fill="#595959"
+                                    d="M256 0c-74.439 0-135 60.561-135 135s60.561 135 135 135 135-60.561 135-135S330.439 0 256 0zM423.966 358.195C387.006 320.667 338.009 300 286 300h-60c-52.008 0-101.006 20.667-137.966 58.195C51.255 395.539 31 444.833 31 497c0 8.284 6.716 15 15 15h420c8.284 0 15-6.716 15-15 0-52.167-20.255-101.461-57.034-138.805z"
+                                />
+                            </svg>
+                        </span>
+                        <input required type="text" className="input" />
+                        <label className="label">Email or Phone</label>
+                    </div>
+
+                    {/* Password Field */}
+                    <div className="field">
+                        <span className="span">
+                            <svg
+                                xmlSpace="preserve"
+                                viewBox="0 0 512 512"
+                                height="20"
+                                width="50"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fill="#595959"
+                                    d="M336 192h-16v-64C320 57.406 262.594 0 192 0S64 57.406 64 128v64H48c-26.453 0-48 21.523-48 48v224c0 26.477 21.547 48 48 48h288c26.453 0 48-21.523 48-48V240c0-26.477-21.547-48-48-48zm-229.332-64c0-47.063 38.27-85.332 85.332-85.332s85.332 38.27 85.332 85.332v64H106.668zm0 0"
+                                />
+                            </svg>
+                        </span>
+                        <input required type="password" className="input" />
+                        <label className="label">Password</label>
+                    </div>
+
+                    <div className="forgot-pass">
+                        <a href="#">Forgot Password?</a>
+                    </div>
+
+                    <button type="submit" className="registration-button">
+                        Register
+                    </button>
+
+                    <div className="sign-up">
+                        Already a member? <a href="#">Login Here</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
